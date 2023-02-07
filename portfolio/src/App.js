a93c0bc3dd3be7cfcc1381695272099637944e47
Kendrameda <gibsoken@gmail.com>
2023-02-07 08:36:53 -0600

    starting code added and started on coding as well

---
+++ portfolio/src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import './App.css';
+import { Header, About, Portfolio, Contact, Resume, Footer } from './components'
+
+function App() {
+
+  const [currentPage, setCurrentPage] = useState("About");
+
+  return (
+    <>
+      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {currentPage === "About" && <About setCurrentPage={setCurrentPage}/>}
+      {currentPage === "Portfolio" && <Portfolio />}
+      {currentPage === "Contact" && <Contact />}
+      {currentPage === "Resume" && <Resume />}
+      <br></br>
+      <Footer />
+      </>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from './Header'
+import Footer from './Footer'
+import About from './About'
+import Contact from './Contact'
+import Portfolio from './Portfolio'
+import Resume from './Resume'
+
+export {Header, Footer, About, Contact, Portfolio, Resume};
